d1bcd900d5fa2c79ebfc83f6933be70d2a716930
almamani <almamani@agetic.gob.bo>
2018-12-03 09:46:35 -0400

    adicion metodo multiplicar

--- prueba.js
+++ prueba.js
@@ -1,7 +1,14 @@
 const sumar = (a,b) => {
-  const suma = a +b; 
-  console.log(suma);
+  const suma = a + b; 
+  console.log(`${a}+${b}=${suma}`);
   return suma;
 };
 
+const multiplicar = (a,b) => {
+  const resultado = a * b;
+  console.log(`${a}*${b}=${resultado}`);
+  return resultado;
+}
+
 sumar(4,5);
+multiplicar(4,5);
